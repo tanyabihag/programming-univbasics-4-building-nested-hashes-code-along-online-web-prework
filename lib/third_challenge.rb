eeff9919bbd32123cc408401d5f054af928c502e
Tanya Bihag <tbihag@gmail.com>
2019-08-08 23:33:03 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,38 +1,21 @@
 def third_challenge
-  epic_tragedy = {
-   :montague => {
-      :patriarch => {
-        :name = ["Lord Montague"]
-        :age = [53]
-        },
-      :matriarch => {
-        :name = ["Lady Montague"]
-        :age = [54]
-        },
-      :hero => {
-        :name = ["Romeo"]
-        :age = [15]
-        :status = ["alive"]
-        },
-      :hero_friends => []
-   }, 
-   :capulet => {
-      :patriarch => {
-        :name = ["Lord capulet"]
-        :age = [50]
-        },
-      :matriarch => {
-        :name = ["Lady capulet"]
-        :age = [51]
-          },
-      :heroine => {
-        :name = ["Juliet"]
-        :age = [15]
-        :status = ["Alive"]
-        },
-      :heroine_friends => []
-   }
-  }
-
-  
-end
+     epic_tragedy = {
+       :montague => {
+          :patriarch => {name: "Lord Montague", age: "53"},
+          :matriarch => {name: "Lady Montague", age: "54"},
+          :hero => {name: "Romeo", age: "15", status: "alive"},
+          :hero_friends => [
+             {name: "Benvolio", age: "17", attitude: "worried"},
+             {name: "Mercutio", age: "18", attitude: "hot-headed"}
+          ]
+       },
+       :capulet => {
+          :patriarch => {name: "Lord Capulet", age: "50"},
+          :matriarch => {name: "Lady Capulet", age: "51"},
+          :heroine => {name: "Juliet", age: "15", status: "alive"},
+          :heroine_friends => [
+              {name: "Steven", age: "30", attitude: "confused"},
+              {name: "Nurse", age: "44", attitude: "worried"}
+          ]
+       }
+    }
